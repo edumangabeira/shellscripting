805144c0baf526fbd619e2da4c993597fca2ce02
Eduardo <edu.mangaba@gmail.com>
2018-08-31 16:10:22 -0300

    hello world

---
+++ hellochrome.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+google-chrome --user-data-dir=/home/YourLocalHere/.CarlosMagno45 
+
+#a simple script to open chrome browser directly from command line as something similar to a virtual box, useful when working with bots, for example
+
