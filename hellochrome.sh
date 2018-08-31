e9e984017c88291b45c7b175f1dc2dc912f890c8
Eduardo Mangabeira <edu.mangaba@gmail.com>
2018-08-31 16:12:41 -0300

    Update hellochrome.sh

--- hellochrome.sh
+++ hellochrome.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-google-chrome --user-data-dir=/home/YourLocalHere/.CarlosMagno45 
+#a simple script to open chrome browser directly from command line as something similar to a virtual box.
+#useful when working with bots, for example,
 
-#a simple script to open chrome browser directly from command line as something similar to a virtual box, useful when working with bots, for example
+#!/bin/bash
+google-chrome --user-data-dir=/home/YourUserHere/.YourScriptLocal 
 
